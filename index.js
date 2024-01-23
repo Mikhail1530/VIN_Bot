f77452d8cac8f79c1c7f472ca1cba6512de5ceb9
Mikhail1530 <michael.nfnr@gmail.com>
2024-01-23 17:44:15 +0400

    bot is done 14

--- index.js
+++ index.js
@@ -43,12 +43,13 @@ const start = () => {
             const url = `report?vin=${msg.text}&format=pdf&reportTemplate=2021`
 
             // const responsePdf = await instance.get(url).then(res => res.data)
-            const responsePdf = await instance.get(url).then(res => res.data).catch(err => err? 'Error' : '')
+            const responsePdf = await instance.get(url).then(res => res.data)
 
 
             console.log(JSON.stringify(responsePdf.data))
             await bot.sendDocument(msg.chat.id, responsePdf)
-            // await bot.sendMessage(msg.chat.id, 'Скачать файл')
+
+
             allRequests += 1
             listUsersUsed[msg.from.first_name] ? listUsersUsed[msg.from.first_name] += 1 : listUsersUsed[msg.from.first_name] = 1
         }
