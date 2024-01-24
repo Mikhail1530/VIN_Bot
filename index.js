e1b6ba320ced1403331052acba567e0e403261e7
Mikhail1530 <michael.nfnr@gmail.com>
2024-01-24 16:17:42 +0400

    bot is done 32

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bot = new TelegramApi(token, {polling: true})
 const tokenVin = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZpcm9ubWVudCI6InRlc3QiLCJ1c2VyIjp7ImlkIjoyMDg1MTEsImVtYWlsIjoiYXV0b3BvZGJlcnUxKzFAZ21haWwuY29tIn0sInZlbmRvciI6eyJpZCI6MjczLCJzdGF0dXMiOiJhY3RpdmUiLCJpcCI6WyIxNzIuMjAuMTAuMyIsIjU0Ljg2LjUwLjEzOSIsIjE4NS4xMTUuNC4xNDciLCIxODUuMTE1LjUuMjgiLCI1LjE4OC4xMjkuMjM2Il19LCJpYXQiOjE3MDYwMTI1NzQsImV4cCI6MTcwODYwNDU3NH0.D5hOhF4CUOcMlyE4meRRPggfnZpKejKgDcHrlAWM6e4'
 const instance = axios.create({
     baseURL: "https://www.clearvin.com/rest/vendor/",
-    // responseType: 'blob',
+    responseType: 'arraybuffer',
     headers: {
         Authorization: `Bearer ${tokenVin}`,
     },
@@ -42,7 +42,9 @@ const start = () => {
         if (match[0].length === 17 && authenticate_users(msg.from.id)) {
             const url = `report?vin=${msg.text}&format=pdf&reportTemplate=2021`
             // const url = `preview?vin=${msg.text}`
-            const responsePdf = await instance.get(url).then(res => res.data)
+            const responsePdf = await instance.get(url).then(res => {
+                return new Blob([res.data])
+            })
             // {
             //     const blobFile = res.data
             //     return URL.createObjectURL(blobFile)
