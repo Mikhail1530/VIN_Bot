441d138e105ef80289d40ee9be07a7c176639d4c
Mikhail1530 <michael.nfnr@gmail.com>
2024-01-24 17:20:00 +0400

    bot is done 36

--- index.js
+++ index.js
@@ -47,9 +47,7 @@ const start = () => {
             //     const blobFile = res.data
             //     return URL.createObjectURL(blobFile)
             // })
-            console.log(responsePdf instanceof Blob)
-            console.log(responsePdf instanceof File)
-            console.log(typeof responsePdf)
+            console.log(responsePdf instanceof ArrayBuffer)
             // await bot.sendDocument(msg.chat.id, responsePdf, {}, {
             //     filename: './Example.pdf',
             //     contentType: 'application/pdf'
